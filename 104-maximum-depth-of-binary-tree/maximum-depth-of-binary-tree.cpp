6ce0135043e76fd3064df836296007378630d56f
phoenixultra <ducphatphoenixultra@gmail.com>
2026-04-01 22:49:44 +0700

    Time: 0 ms (100.00%) | Memory: 19.1 MB (13.37%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+
+        int d = maxDepth(root->left);
+        int s =  maxDepth(root->right) ;
+        int res = max(d,s) + 1;
+        return res ;
+
+    }
+};
